68bd32afa7e865ecd0c14def13d6f5369c8ce3d6
Ben Buscarino <buscari3@msu.edu>
2021-08-05 22:00:51 -0400

    Remove debug

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,8 +1,6 @@
 const navbar = document.querySelector("nav");
 
 document.addEventListener('scroll', (e) => {
-    console.log(e);
-    console.log(window.scrollY);
     if (window.scrollY > 200) {
         if (navbar.classList.contains("bg-opacity-50")) {
             navbar.classList.remove("bg-opacity-50");
